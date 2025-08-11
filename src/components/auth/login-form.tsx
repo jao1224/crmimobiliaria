e4242e6dcfe8013bc9d1c3af6e2e87572d8a8b8c
João Fernando Fernandes Frota Lima <fernandojoao072@gmail.com>
2025-08-11 15:30:21 +0000

    coloque ele presetado

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -32,8 +32,8 @@ export function LoginForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: "",
-      password: "",
+      email: "admin@example.com",
+      password: "admin",
     },
   });
 
