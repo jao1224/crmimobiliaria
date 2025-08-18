57074368f19bca0054b79e94704e92938f49771e
Ítalo Lima <italolimac01@gmail.com>
2025-08-18 11:54:50 +0000

    nao estao aparecendo os imoveis ao atulizar a pagina

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKnrPMARuUnHjjUxezLdLg2ZKW9WI2PHs",
+  apiKey: "AIzaSyCKnrPMARuUnHjjUxezLdLg2ZKW9WI2PHk",
   authDomain: "crmimobiliararia.firebaseapp.com",
   projectId: "crmimobiliararia",
   storageBucket: "crmimobiliararia.appspot.com",
