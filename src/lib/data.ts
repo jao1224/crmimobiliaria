b1edf417efb0989348474f3a1ace29888dd20c62
João Fernando Fernandes Frota Lima <fernandojoao072@gmail.com>
2025-08-22 16:21:13 +0000

    voce esta fazendo assim ? Aqui está a verificação e a descrição do mecan

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,6 +4,7 @@
 
 
 
+
 import { db, storage } from './firebase';
 import { collection, getDocs, addDoc, doc, updateDoc, writeBatch, serverTimestamp, query, orderBy, limit, where, getDoc, setDoc, deleteDoc } from 'firebase/firestore';
 import { ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage';
@@ -1056,6 +1057,7 @@ export const updateActivityStatus = async (activityId: string, newStatus: Activi
     
 
     
+
 
 
 
