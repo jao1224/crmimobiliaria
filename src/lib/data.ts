e1a36d47c5e7e3628374f944151f1f4b14a4c0c5
João Fernando Fernandes Frota Lima <fernandojoao072@gmail.com>
2025-08-30 12:45:19 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button>
    
    Relevant

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 
 
 import { db, storage } from './firebase';
-import { collection, getDocs, addDoc, doc, updateDoc, writeBatch, serverTimestamp, query, orderBy, limit, where, getDoc, setDoc, deleteDoc } from 'firebase/firestore';
+import { collection, getDocs, addDoc, doc, updateDoc, writeBatch, serverTimestamp, query, orderBy, limit, where, getDoc, setDoc, deleteDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
 import { ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage';
 import { type User as FirebaseUser } from 'firebase/auth';
 
